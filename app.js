e05b348da0b5bc14a71df5c8850064b3bca9492d
Delyan Todorov <IDIliev18@codingburgas.bg>
2020-12-16 19:19:45 +0200

    Fix routes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static("./public"));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
-});
+})
 
 app.get('/singleplayer', function(req, res) {
     res.sendFile(__dirname + '/public/singleplayer.html');
