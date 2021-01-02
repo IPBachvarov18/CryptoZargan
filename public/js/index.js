198498fd9adc7717314eee9dba4c8c72700f4312
Delyan Petrov <ipbachvarov18@codingburgas.bg>
2021-01-02 16:51:59 +0200

    Add accordion menus to rules.html
    :wq

--- public/js/index.js
+++ public/js/index.js
@@ -17,3 +17,27 @@ $(document).ready(function() {
     let lang = Cookies.get("language");
     translateLabel(lang);
 });
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+
+  acc[i].addEventListener("click", function() {
+
+    this.classList.toggle("active");
+     let panel = this.nextElementSibling;
+
+    if (panel.style.display === "block") 
+    {
+      panel.style.display = "none";
+    } 
+
+    else 
+    {
+      panel.style.display = "block";
+    }
+
+  });
+}
