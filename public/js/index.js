bcbce40bcd2c68722c72666d4d8c9e907edeedfc
Delyan Petrov <ipbachvarov18@codingburgas.bg>
2021-01-11 10:56:38 +0200

    Add text design to rules.html
    * Make better translation
    * Fix error404 page

--- public/js/index.js
+++ public/js/index.js
@@ -16,17 +16,17 @@ $(document).ready(function () {
 	translateLabel(lang);
 });
 
-let acc = document.getElementsByClassName("accordion");
+// let acc = document.getElementsByClassName("accordion");
 
-for (let i = 0; i < acc.length; i++) {
-	acc[i].addEventListener("click", function () {
-		this.classList.toggle("active");
-		let panel = this.nextElementSibling;
+// for (let i = 0; i < acc.length; i++) {
+// 	acc[i].addEventListener("click", function () {
+// 		this.classList.toggle("active");
+// 		let panel = this.nextElementSibling;
 
-		if (panel.style.display === "block") {
-			panel.style.display = "none";
-		} else {
-			panel.style.display = "block";
-		}
-	});
-}
+// 		if (panel.style.display === "block") {
+// 			panel.style.display = "none";
+// 		} else {
+// 			panel.style.display = "block";
+// 		}
+// 	});
+// }
